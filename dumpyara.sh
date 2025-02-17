d90430cf9b2c72b09f5fd7db2d98f5a40697c019
Leonardo Ledda <leonardoledda@gmail.com>
2025-02-17 11:58:57 +0100

    dumpyara: Adapt for my usage
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- dumpyara.sh
+++ dumpyara.sh
@@ -52,7 +52,7 @@ else
     [[ -e "$URL" ]] || { echo "Invalid Input" && exit 1; }
 fi
 
-ORG=Jiovanni-dump #your GitHub org name
+ORG=leddaz-dump-stash #your GitHub org name
 FILE=$(echo ${URL##*/} | inline-detox)
 EXTENSION=$(echo ${URL##*.} | inline-detox)
 UNZIP_DIR=${FILE/.$EXTENSION/}
@@ -322,7 +322,7 @@ fi
 # Telegram channel
 TG_TOKEN=$(< "$PROJECT_DIR"/.tgtoken)
 if [[ -n "$TG_TOKEN" ]]; then
-    CHAT_ID="@jiovanni_dumps"
+    CHAT_ID="@leddaz_dumpyara_dump"
     commit_head=$(git log --format=format:%H | head -n 1)
     commit_link="https://github.com/$ORG/$repo/commit/$commit_head"
     echo -e "Sending telegram notification"
